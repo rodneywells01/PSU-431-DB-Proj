686da1221958c251091b62edb9e3069ffb363ddc
sbaskin <s.baskin2017@gmail.com>
2017-03-31 23:55:24 -0400

    Created Overall website shell. This includes page layout with the addition of four new pages.

--- mysite/root/urls.py
+++ mysite/root/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'newaccount/', views.newaccount, name='newaccount'),
+	url(r'searchDiseases/', views.searchDiseases, name='searchDiseases'),
+	url(r'startDiagnosis/', views.startDiagnosis, name='startDiagnosis'),
+	url(r'aboutUs/', views.aboutUs, name='aboutUs'),
+	url(r'contact/', views.contact, name='contact'),
 ]
--- mysite/root/views.py
+++ mysite/root/views.py
@@ -12,3 +12,23 @@ def newaccount(request):
 	template = loader.get_template('root/newaccount.html')
 	context = { }
 	return HttpResponse(template.render(context, request))	
+
+def searchDiseases(request):
+	template = loader.get_template('root/searchDiseases.html')
+	context = { }
+	return HttpResponse(template.render(context, request))	
+
+def startDiagnosis(request):
+	template = loader.get_template('root/startDiagnosis.html')
+	context = { }
+	return HttpResponse(template.render(context, request))	
+
+def aboutUs(request):
+	template = loader.get_template('root/aboutUs.html')
+	context = { }
+	return HttpResponse(template.render(context, request))	
+
+def contact(request):
+	template = loader.get_template('root/contact.html')
+	context = { }
+	return HttpResponse(template.render(context, request))	
