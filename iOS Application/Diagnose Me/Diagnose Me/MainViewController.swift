35e73f9d5ce2414175ab74e6765685377534c93c
Luke <l.porupski@outlook.com>
2017-04-03 14:16:30 -0400

    Adding iOS application project to repo

---
+++ iOS Application/Diagnose Me/Diagnose Me/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MainViewController.swift
+//  Diagnose Me
+//
+//  Created by Luke Porupski on 3/1/17.
+//  Copyright © 2017 Luke Porupski. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var searchButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        searchButton.layer.cornerRadius = 5
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
