ff95eb7b314d1c4f7a5873ef968b9f41162a61cc
avermeulen14 <andreasvermeulen45@gmail.com>
2017-04-10 22:57:53 -0400

    minor fixes.

--- mysite/root/models.py
+++ mysite/root/models.py
@@ -57,6 +57,9 @@ class SuffersFrom(models.Model):
 	symptom = models.ForeignKey(Symptom, on_delete=models.CASCADE)
 	duration = models.DurationField(default=0)
 
+
+#Need to adjust - to fit schema
+
 class Doctors(models.Model):
     name = models.CharField(max_length = 40)
 	doctor_id = models.AutoField(primary_key=true) #potential change
@@ -65,85 +68,84 @@ class Doctors(models.Model):
 	def __str__(self):
     		return self.name
 
-#Need to adjust - to fit schema
 class Bids(models.Model):
 	amount = models.DecimalField(max_digits = 5, decimal_places=2)
 	user_id = models.IntegerField(max_length=9)
 	auction_id = models.IntegerField(max_length=9)
 	#add keys
-class Symptoms(models.Models):
+class Symptoms(models.Model):
 	symptom_id = models.IntegerField(max_length=9)
 	area_of_body = models.CharField(max_length = 40)
 	description = models.CharField(max_length = 200)
 	illness_id = models.IntegerField(max_length=9)
 	#add keys
 
-class Treatments(models.Models):
+class Treatments(models.Model):
 	remedy_id = models.DecimalField(max_digits=9, decimal_places=0)
 	illness_id = models.IntegerField(max_length=9)
 	#add keys.
 
-class Auctions(models.Models):
+class Auctions(models.Model):
 	auction_id = models.IntegerField(max_length=9)
 	reserve_price = models.DecimalField(max_digits=5, decimal_places=2)
-	end_date = models.DateTimeField(#add field)
+	end_date = models.DateField(auto_now_add=True)
 	remedy_id = models.IntegerField(max_length=9)
 	#add keys
 
-class Accepted_Insurance(models.Models):
+class Accepted_Insurance(models.Model):
 	doctor_id = models.IntegerField(max_length=9)
-	insurance_name = CharField(max_length = 40)
+	insurance_name = models.CharField(max_length = 40)
 	#add key information
 
-class Payment_Information(models.Models):
+class Payment_Information(models.Model):
 	address = models.CharField(max_length=60)
-	card_number = CharField(max_length=20)
-	user_id = IntegerField(max_length= 9)
+	card_number = models.CharField(max_length=20)
+	user_id = models.IntegerField(max_length= 9)
 	#add keys
 
-class Diagnoses(models.Models):
+class Diagnoses(models.Model):
 	user_id = models.IntegerField(max_length=9)
 	illness_id = models.IntegerField(max_length=9)
 	likelihood = models.PositiveIntegerField(max_length=5)
 	#add keys.
 
-class Remedy_Ratings(models.Models):
+class Remedy_Ratings(models.Model):
 	remedy_id = models.IntegerField(max_length=9)
 	rating = models.DecimalField(max_digits=5, decimal_places=2)
 	comment = models.CharField(max_length=500)
 	user_id = models.IntegerField(max_length=9)
 	#add keys.
 
-class Doctor_Ratings(models.Models):
+class Doctor_Ratings(models.Model):
 	doctor_id = models.IntegerField(max_length=9)
 	rating = models.DecimalField(max_digits=5, decimal_places=2)
 	comment = models.CharField(max_length=500)
 	#add keys.
 
-class Orders(models.Models):
+class Orders(models.Model):
 	order_id = models.IntegerField(max_length=9)
-	order_date = models.DateTimeField(#add field)
+	order_date = models.DateTimeField(auto_now_add=true)
 	user_id = models.IntegerField(max_length=9)
-	repurchase_date = models.DateTimeField(#add field)
+	repurchase_date = models.DateTimeField(auto_now_add=true)
 	#add keys
 
-class Order_Items(models.Models):
+class Order_Items(models.Model):
 	order_id = models.IntegerField(max_length=9)
 	remedy_id = models.IntegerField(max_length=9)
 	#add keys
-class Cart_items(models.Models):
+class Cart_items(models.Model):
 	user_id = models.IntegerField(max_length=9)
 	remedy_id = models.IntegerField(max_legth=9)
 	#add keys
 
-class Order_Deliveries(models.Models):
+class Order_Deliveries(models.Model):
 	user_id = models.IntegerField(max_length=9)
 	order_id = models.IntegerField(max_length=9)
 	address = models.CharField(max_length= 60)
 	status = models.CharField(max_legth=30)
 	#add keys
 
-class Auction_deliveries(models.Models):
+class Auction_deliveries(models.Model):
 	user_id = models.IntegerField(max_length=9)
 	auction_id = models.IntegerField(max_length=9)
 	address = models.CharField(max_legth=60)
